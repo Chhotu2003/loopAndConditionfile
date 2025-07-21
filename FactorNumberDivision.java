b08e26252f4e4e09565eb12b6977292a6683d1ec
Chhotu kumar <168346582+Chhotu2003@users.noreply.github.com>
2025-07-21 13:37:20 +0000

    Factor of Number Division  in java code

---
+++ FactorNumberDivision.java
@@ -0,0 +1,13 @@
+public class FactorNumberDivision {
+    public static void main(String[] args) {
+        int n=12;
+        for(int i=1;i<=n;i++){
+            if(n%i==0)
+            System.out.println(i);
+        }
+    }
+}
+
+////// ans /////////////
+/// input = 12
+/// output = 1 2 3 4 6 12 
